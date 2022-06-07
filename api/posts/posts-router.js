6acb4b62d7811bb67af6220d172b47b489fd3eff
John Ruddock <john.jxr@pm.me>
2022-06-07 16:57:32 -0400

    fetch comments for a Post in place, MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -154,4 +154,34 @@ router.delete('/:id', (req, res) => {
     
 })
 
+router.get('/:id/comments', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if(post == null) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            });
+            return;
+        }
+        Posts.findPostComments(req.params.id)
+        .then(comments => {
+            res.json(comments)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The comments information could not be retrieved",
+                err: err.message,
+                stack: err.stack
+        })
+    })
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message,
+            stack: err.stack
+        })
+    })
+})
+
 module.exports = router;
