51c84005463e3aeca188b9de077439b7f064eefe
John Ruddock <john.jxr@pm.me>
2022-06-07 15:10:48 -0400

    get posts route in place

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,8 +4,20 @@ const express = require('express');
 
 const router = express.Router();
 
+const Posts = require('./posts-model')
+
 router.get('/', (req, res) => {
-    res.send('Hello from posts Router!')
+    Posts.find()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message,
+            stack: err.stack
+        })
+    })
 })
 
 module.exports = router;
